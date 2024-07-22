42516e9fa6fef2cb95c859d9203a23c2e166cd18
Andri Schatz <andri.schatz@gmail.com>
2024-07-22 17:53:42 +0200

    i learned a lot

---
+++ message_passing/target/debug/deps/message_passing-9df88a073f4ed1ee.d
@@ -0,0 +1,5 @@
+/Users/andrischatz/dev/rust_book_tut/message_passing/target/debug/deps/message_passing-9df88a073f4ed1ee: src/main.rs
+
+/Users/andrischatz/dev/rust_book_tut/message_passing/target/debug/deps/message_passing-9df88a073f4ed1ee.d: src/main.rs
+
+src/main.rs:
---
+++ message_passing/target/debug/message_passing.d
@@ -0,0 +1,1 @@
+/Users/andrischatz/dev/rust_book_tut/message_passing/target/debug/message_passing: /Users/andrischatz/dev/rust_book_tut/message_passing/src/main.rs
